630791bc62f474d78fe66eadcc90a8e5b9b2235f
Skin <rizqinnazhif@gmail.com>
2019-11-14 19:59:24 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -6,7 +6,7 @@ function basicTeenager(age) {
 
 function teenager(age) {
   if(age > 12 && age < 20){
-    return 'You are a teenager';
+    return 'You are a teenager!';
   } else {
     return 'You are not a teenager';
   }
