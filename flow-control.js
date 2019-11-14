546644cc8bfa4bb0cb427402a6a7bd672efa8251
Skin <rizqinnazhif@gmail.com>
2019-11-14 20:15:48 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -31,6 +31,24 @@ function switchAge(age) {
     case 13:
       return "You are a teenager"
       break;
+    case 14:
+      return "You are a teenager"
+      break;
+    case 15:
+      return "You are a teenager"
+      break;
+    case 16:
+      return "You are a teenager"
+      break;
+    case 17:
+      return "You are a teenager"
+      break;
+    case 18:
+      return "You are a teenager"
+      break;
+    case 19:
+      return "You are a teenager"
+      break;
     default:
       return "You have an age"
   }
